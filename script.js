84c3b68cbfd39b292df32ab623e75f4160d30529
HagenStruve <Struve94@web.de>
2022-08-21 21:21:31 +0200

    commit 2

---
+++ script.js
@@ -0,0 +1,37 @@
+let questions = [
+    {
+    "Frage" : "Wer ist hier der Boss?",
+    "Antwort_1": "Ich",
+    "Antwort_2": "Du",
+    "Antwort_3": "Er",
+    "Antwort_4": "Sie",
+    "Richtige Antwort": 2
+},
+{
+    "Frage" : "Wer ist hier der Boss?",
+    "Antwort_1": "Ich",
+    "Antwort_2": "Du",
+    "Antwort_3": "Er",
+    "Antwort_4": "Sie",
+    "Richtige Antwort": 2
+},
+{
+    "Frage" : "Wer ist hier der Boss?",
+    "Antwort_1": "Ich",
+    "Antwort_2": "Du",
+    "Antwort_3": "Er",
+    "Antwort_4": "Sie",
+    "Richtige Antwort": 2
+},
+{
+    "Frage" : "Wer ist hier der Boss?",
+    "Antwort_1": "Ich",
+    "Antwort_2": "Du",
+    "Antwort_3": "Er",
+    "Antwort_4": "Sie",
+    "Richtige Antwort": 2
+},
+];
+
+
+function init() {}
